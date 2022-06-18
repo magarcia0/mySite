38c910f050213ee8a4886e771c747c8bb3adcc79
Marco <>
2022-06-18 09:22:53 -0700

    more seo

--- components/header.js
+++ components/header.js
@@ -8,13 +8,18 @@ export default function Header() {
       <Head>
         <meta charSet="utf-8" />
         <link rel="icon" href="/favicon.ico" />
-        <meta name="description" content="Fitness Workout Builder" />
-        <meta property="og:title" content={siteTitle} />
+        <meta name="description" content="Marco Garcia Software Developer" />
+        <link rel="canonical" href="https://marcoagarcia.com/"></link>
+        <meta property="og:title" content="Marco Garcia" />
+        <meta property="og:site_name" content="Marco Garcia"></meta>
+        <meta property="og:url" content="https://marcoagarcia.com/"></meta>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="twitter:title" content="Marco Garcia"></meta>
+        <meta name="twitter:url" content="https://marcoagarcia.com/"></meta>
         <NextSeo
         title="Marco Garcia"
         titleTemplate="Marco Garcia | Next SEO"
-        description="A website for Marco A Garcia to show projects he's worked on"
+        description="A website for Marco Garcia to show his development skills"
         canonical="https://marcoagarcia.com/"
         />
       </Head>
